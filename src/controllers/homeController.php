3eec5b47e201ff852ddc252ca7573bd7a863df67
Julien Damiano <julien.damiano64@gmail.com>
2024-07-05 09:45:52 +0200

    fixed XSS in searchbar in twig views

--- src/controllers/homeController.php
+++ src/controllers/homeController.php
@@ -1,8 +1,17 @@
 <?php
 
-$data['movies'] = getMovies();
-$data['get'] = $_GET;
+require_once '../src/includes/functions.php';
 
-// if (!empty($_GET['search'])) {
-//     searchByName($data['movies']['title'], $_GET['search']);
-// }
+$data['movies'] = getMovies();
+$data['get'] = array_map(function ($value) {
+    return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+}, $_GET);
+
+if (!empty($data['get']['search'])) {
+    $search = $data['get']['search'];
+    $data['movies'] = array_filter($data['movies'], function ($movie) use ($search) {
+        return searchByName($movie->title, $search); // Utilisation de la notation objet
+    });
+}
+
+echo $twig->render('home.twig', $data);
